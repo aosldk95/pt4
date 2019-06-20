a523a065091ccc5f35dd0d9dbb5c394adfac31bd
강병안 <aosldk95@naver.com>
2019-06-20 16:39:38 +0900

    이벤트

--- pt4/Program.cs
+++ pt4/Program.cs
@@ -525,7 +525,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
 namespace ConsoleApp3
 {
     delegate void DelegateType();
@@ -558,4 +558,51 @@ namespace ConsoleApp3
 
 
 }
+*/
 
+namespace ConsoleApp3
+{
+    delegate void DelegateType(string Message);
+
+    class A
+    {
+        public event DelegateType EventHandler;
+        public void Func(string Message)
+        {
+            EventHandler(Message);
+        }
+    }
+
+    class B
+    {
+        public void PrintA(string Message)
+        {
+            Console.WriteLine(Message);
+        }
+
+        public void PrintB(string Message)
+        {
+            Console.WriteLine(Message);
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            A Test1 = new A();
+            B Test2 = new B();
+
+            Test1.EventHandler += new DelegateType(Test2.PrintA);
+            Test1.EventHandler += new DelegateType(Test2.PrintB);
+            Test1.Func("Good!!!");
+            Test1.EventHandler -= Test2.PrintB;
+            Test1.Func("Hi~~!");
+            Test1.EventHandler -= Test2.PrintA;
+
+            Test1.EventHandler += Test2.PrintA;
+            Test1.EventHandler += Test2.PrintB;
+
+            Test1.Func("Hello World");
+        }
+    }
+}
