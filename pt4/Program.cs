0cc20ff9aee39e4e7d0b8f6c32e9511a744cccc5
강병안 <aosldk95@naver.com>
2019-06-20 15:31:29 +0900

    delegate2

--- pt4/Program.cs
+++ pt4/Program.cs
@@ -484,6 +484,7 @@ namespace pt4
     
 }
 */
+/*
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -516,5 +517,45 @@ namespace ConsoleApp3
     }
 
     
+}
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    delegate void DelegateType();
+
+    class A
+    {
+        public void PrintA()
+        {
+            Console.WriteLine("PrintA");
+        }
+
+        public void PrintB()
+        {
+            Console.WriteLine("PrintB");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A Test = new A();
+            DelegateType DelFunc = Test.PrintA;
+            DelFunc += Test.PrintB;
+            DelFunc();
+            DelFunc -= Test.PrintB;
+            DelFunc();
+        }
+    }
+
+
 }
 
